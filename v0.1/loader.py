8a175104add329846819c23f5f4b2a93e7173be0
TonyCross <starovoitov.anton@gmail.com>
2016-12-30 14:13:22 +0500

    Создан класс, который скачивает ссылку и сохраняет в файл

--- v0.1/loader.py
+++ v0.1/loader.py
@@ -1,1 +1,17 @@
 # TODO Скачивание ссылки
+# TODO Читаем библиотеку urllib
+import urllib.request
+
+
+class Downloader():
+    def __init__(self):
+        pass
+
+    def download(self, link):
+        response = urllib.request.urlopen('{}'.format(link))
+        file = open('test.txt', 'w')
+        file.write(str(response.read(), encoding='UTF-8'))
+
+
+first_loader = Downloader()
+first_loader.download('https://habrahabr.ru/post/134863/')
