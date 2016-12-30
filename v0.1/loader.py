04682953e4a348b1025c5407ccd9ea57252931f5
TonyCross <starovoitov.anton@gmail.com>
2016-12-30 22:05:49 +0500

    Добавил комменты

--- v0.1/loader.py
+++ v0.1/loader.py
@@ -1,21 +1,19 @@
-# TODO Скачивание ссылки
-# TODO Читаем библиотеку urllib
 import urllib.request, re
 from bs4 import BeautifulSoup
 
 def parse_link(link):
     dirty_link = urllib.request.urlopen('{}'.format(link)).read()
     soup = BeautifulSoup(dirty_link, "lxml")
-    text_title = soup.findAll('title')
-    text_news = soup.findAll('p')
-    dirty_text = str(text_title) + '\n' + '\n' + str(text_news)
+    text_title = soup.findAll('title') # Вытаскиваем заголовок
+    text_news = soup.findAll('p') # вытаскиваем текст статьи
+    dirty_text = str(text_title) + '\n' + '\n' + str(text_news) # соединяем
     return dirty_text
 
 
 def cleaner(dirty_text):
-    clean_text = re.sub(r'(\<(/?[^>]+)>)', '', str(dirty_text))
-    clean_text2 = re.sub(r'\.,', '\n\n', str(clean_text))
-    clean_text3 = re.sub(r'\[', ' ', str(clean_text2))
+    clean_text = re.sub(r'(\<(/?[^>]+)>)', '', str(dirty_text)) # Убираем теги
+    clean_text2 = re.sub(r'\.,', '\n\n', str(clean_text)) # Добавляем отступ у абзацев
+    clean_text3 = re.sub(r'\[', ' ', str(clean_text2)) # Убираем скобки лишние
     clean_text4 = re.sub(r'\]', '', str(clean_text3))
     print(clean_text4)
     return clean_text4
