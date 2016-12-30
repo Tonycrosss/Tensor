6a76d5adf1e9ef9aec8a2046cc828198977dea84
TonyCross <starovoitov.anton@gmail.com>
2016-12-30 21:28:51 +0500

    Качает, парсит, чистит, записывает в файл

--- v0.1/loader.py
+++ v0.1/loader.py
@@ -1,17 +1,35 @@
 # TODO Скачивание ссылки
 # TODO Читаем библиотеку urllib
-import urllib.request
+import urllib.request, re
+from bs4 import BeautifulSoup
+
+def parse_link(link):
+    dirty_link = urllib.request.urlopen('{}'.format(link)).read()
+    soup = BeautifulSoup(dirty_link, "lxml")
+    text_title = soup.findAll('title')
+    text_news = soup.findAll('p')
+    dirty_text = str(text_title) + '\n' + '\n' + str(text_news)
+    return dirty_text
 
 
-class Downloader():
-    def __init__(self):
-        pass
+def cleaner(dirty_text):
+    clean_text = re.sub(r'(\<(/?[^>]+)>)', '', str(dirty_text))
+    clean_text2 = re.sub(r'\.,', '\n\n', str(clean_text))
+    clean_text3 = re.sub(r'\[', ' ', str(clean_text2))
+    clean_text4 = re.sub(r'\]', '', str(clean_text3))
+    print(clean_text4)
+    return clean_text4
 
-    def download(self, link):
-        response = urllib.request.urlopen('{}'.format(link))
-        file = open('test.txt', 'w')
-        file.write(str(response.read(), encoding='UTF-8'))
+def text_writer(clean_text):
+    #TODO Имя файла должно быть как ссылка
+    file = open('lenta.txt', 'w')
+    file.write(clean_text)
 
 
-first_loader = Downloader()
-first_loader.download('https://habrahabr.ru/post/134863/')
+
+
+
+first_link = parse_link('https://lenta.ru/news/2016/12/30/otkaz/')
+first_clear_text = cleaner(first_link)
+text_writer(first_clear_text)
+
