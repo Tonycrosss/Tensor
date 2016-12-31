05a6579f4a70d124913d97d4f77060566eae2ab2
TonyCross <starovoitov.anton@gmail.com>
2016-12-31 05:58:03 +0500

    Добавлен formatter(), которая форматирует строки в текстовом файле по ширине (80 символов)

--- v0.1/loader.py
+++ v0.1/loader.py
@@ -6,16 +6,17 @@ def parse_link(link):
     soup = BeautifulSoup(dirty_link, "lxml")
     text_title = soup.findAll('title') # Вытаскиваем заголовок
     text_news = soup.findAll('p') # вытаскиваем текст статьи
-    dirty_text = str(text_title) + '\n' + '\n' + str(text_news) # соединяем
+    dirty_text = str(text_news) # соединяем
     return dirty_text
 
 
 def cleaner(dirty_text):
+    #TODO Написать регулярку, которая будет очищать все и сразу
+
     clean_text = re.sub(r'(\<(/?[^>]+)>)', '', str(dirty_text)) # Убираем теги
-    clean_text2 = re.sub(r'\.,', '\n\n', str(clean_text)) # Добавляем отступ у абзацев
+    clean_text2 = re.sub(r'\.,', '\n', str(clean_text)) # Меняем точку с запятой на отступ
     clean_text3 = re.sub(r'\[', ' ', str(clean_text2)) # Убираем скобки лишние
     clean_text4 = re.sub(r'\]', '', str(clean_text3))
-    print(clean_text4)
     return clean_text4
 
 def text_writer(clean_text):
@@ -23,6 +24,24 @@ def text_writer(clean_text):
     file = open('lenta.txt', 'w')
     file.write(clean_text)
 
+def formatter():
+    file = open('lenta.txt', 'r')
+    text = file.readlines()
+    for line in text:
+        spl_text = line.split(' ')
+        row = []
+        result = []
+        for word in spl_text:
+            if len(' '.join(row)) <= 80:
+                row.append(word)
+            else:
+                result.append(row)
+                row = []
+                row.append(word)
+        result.append(row)  # Цепляем последне слова, потому что до else не доходит
+        file = open('newnew', 'a', encoding='utf-8')
+        for row in result:
+            file.write(str(' '.join(row) + '\n'))
 
 
 
@@ -30,4 +49,5 @@ def text_writer(clean_text):
 first_link = parse_link('https://lenta.ru/news/2016/12/30/otkaz/')
 first_clear_text = cleaner(first_link)
 text_writer(first_clear_text)
+formatter()
 
