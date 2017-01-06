3c3d4155e4c4ab25a36bd707dfbf732f6f5b9d40
TonyCross <starovoitov.anton@gmail.com>
2017-01-07 03:46:42 +0500

    Добавлены комментарии

--- v0.4/main.py
+++ v0.4/main.py
@@ -12,9 +12,9 @@ from tclass import tenslinker
 if __name__ == '__main__':
     user_link = input('Введите ссылку \n')
     print('Смотрим Вашу страницу... \n')
-    tenslinker = tenslinker.TensLinker(user_link)
-    clean_text = tenslinker.parse_link()
-    tenslinker.text_writer(clean_text)
+    tenslinker = tenslinker.TensLinker(user_link)  # Парсим ссылку
+    clean_text = tenslinker.parse_link()  # получаем чистый текст
+    tenslinker.text_writer(clean_text)   # Создаем нужные папки и вписываем текст
     print('Программа выполнена!')
 
 
