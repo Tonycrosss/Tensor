188c93032e6dff4a7dc6b92bbe7cc63e656b64ab
TonyCross <starovoitov.anton@gmail.com>
2017-01-07 03:45:30 +0500

    Добавлены комментарии

--- v0.3/tclass/tenslinker.py
+++ v0.3/tclass/tenslinker.py
@@ -9,11 +9,11 @@ class TensLinker():
 
     def parse_link(self):
         print('Начинаем парсить Вашу страницу... \n')
-        dirty_link = urllib.request.urlopen('{}'.format(self.user_link)).read()
-        soup = BeautifulSoup(dirty_link, "lxml")
+        dirty_link = urllib.request.urlopen('{}'.format(self.user_link)).read()   # Читаем ссылку
+        soup = BeautifulSoup(dirty_link, "lxml")  # используя *суп* парсим
         text_title = soup.findAll('title')  # Вытаскиваем заголовок
         text_news = soup.findAll('p')  # вытаскиваем текст статьи
-        dirty_text = str(text_title) + '\n' + str(text_news)  # соединяем
+        dirty_text = str(text_title) + '\n' + str(text_news)  # соединяем заголовок и текст
         print('Парсинг страницы завершен! \n')
         print('Начинаем чистить страницу от ненужных символов... \n')
         clean_text = re.sub(r'(\<(/?[^>]+)>)', '', str(dirty_text))  # Убираем теги
@@ -26,14 +26,14 @@ class TensLinker():
     def text_writer(self, clean_text):
         # TODO проверка на существование папки и не выдавать ошибку
         print('Создаем папки для хранения чистого текста... \n')
-        os.makedirs(re.sub(r'http\S?://(www.)?', '', self.user_link))
-        os.chdir(re.sub(r'http\S?://(www.)?', '', self.user_link))
-        file = open('temp.txt', 'w')
+        os.makedirs(re.sub(r'http\S?://(www.)?', '', self.user_link))  # Создаем нужные папки (с одноименным названием)
+        os.chdir(re.sub(r'http\S?://(www.)?', '', self.user_link))  # Переходим в новую директорию
+        file = open('temp.txt', 'w')   # создаем временный текстовый файл
         file.write(clean_text)
         file = open('temp.txt', 'r')
         print('Записываем чистый текст в ./index.txt \n')
         text = file.readlines()
-        for line in text:
+        for line in text:   # разбиваем статью по 80 символов + перенос по словам
             spl_text = line.split(' ')
             row = []
             result = []
@@ -48,4 +48,4 @@ class TensLinker():
             file = open('index.txt', 'a', encoding='utf-8')
             for row in result:
                 file.write(str(' '.join(row) + '\n'))
-        os.remove('temp.txt')
+        os.remove('temp.txt')  # удаляем временный файл
